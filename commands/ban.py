db057cf3b7584b745657dbbf17ce3aadcb8410eb
Shiny Miner <falanshhitkedia123@gmail.com>
2025-06-09 17:08:06 +0530

    moderation is 2

--- commands/ban.py
+++ commands/ban.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Moderation(commands.Cog):
+class Moderation2(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.warns = {}  # Simple in-memory warning tracker
