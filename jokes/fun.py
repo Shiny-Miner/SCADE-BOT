5dba2831de47cf2119b58f30035e708938a4e3f2
Shiny Miner <falanshhitkedia123@gmail.com>
2025-06-12 17:47:47 +0530

    Fixed error!

--- jokes/fun.py
+++ jokes/fun.py
@@ -3,6 +3,7 @@ import discord
 import random
 import aiohttp
 
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 GROQ_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
 
